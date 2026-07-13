ccf418b21cfb07b8310a768dd668b49d97774e1f
Devansh Avchat <devanshavchat8208@gmail.com>
2026-07-13 23:32:27 +0530

    Time: 0 ms (100%), Space: 52.5 MB (18.1%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > count){
+                return false;
+            }
+            count = max(count, i + nums[i]);
+        }
+        return true;
+    }
+};
