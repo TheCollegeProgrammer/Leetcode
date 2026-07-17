fa44a8afe48d73e6de68e3615465431e10668be1
Devansh Avchat <devanshavchat8208@gmail.com>
2026-07-17 23:41:10 +0530

    Time: 103 ms (16.87%), Space: 124.8 MB (6.29%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,12 +4,12 @@ public:
         unordered_map<int, int> mp;
 
         for(int i = 0; i < nums.size(); i++){
-            if(mp.find(nums[i]) != mp.end()){
-                if(i - mp[nums[i]] <= k){
+            if(mp.find(nums[i]) != mp.end()){ // iidhar map mai find kiya ki Interger exist karta hai ki nahi
+                if(i - mp[nums[i]] <= k){ // yaha distance calculate karre hai
                     return true;
                 }
             }
-            mp[nums[i]] = i;
+            mp[nums[i]] = i; // if found new int, add its index to map
         }
     return false;
     }
