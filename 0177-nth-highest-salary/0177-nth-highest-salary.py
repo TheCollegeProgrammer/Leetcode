a7c3db278c97e0c57c4e44ca32cf8b8705e7f356
Devansh Avchat <devanshavchat8208@gmail.com>
2026-08-31 21:41:15 +0530

    Time: 284 ms (42.89%), Space: 66.8 MB (80.75%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.py
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -1,18 +1,19 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    # Get unique salaries sorted in descending order
+    unique_salaries = employee['salary'].drop_duplicates().sort_values(ascending=False)
 
-    # Drop any duplicate salary values to avoid counting duplicates as separate salary ranks
-    unique_salaries = employee['salary'].drop_duplicates()
+    # If N is invalid (negative or zero), return "null" as a string
+    if N <= 0:
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
 
-    # Sort the unique salaries in descending order and get the Nth highest salary
-    sorted_salaries = unique_salaries.sort_values(ascending=False)
+    # If N is larger than the number of unique salaries, return None
+    if len(unique_salaries) < N:
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
 
-    # If N exceeds the number of unique salaries, return None
-    if N > len(sorted_salaries):
-        return pd.DataFrame({'Nth Highest Salary': [None]})
-    
-    # Get the Nth highest salary from the sorted salaries
-    nth_highest = sorted_salaries.iloc[N - 1]
-    
-    return pd.DataFrame({'Nth Highest Salary': [nth_highest]})
+    # Get the N-th highest salary
+    nth_salary = unique_salaries.iloc[N-1]
+
+    return pd.DataFrame({f'getNthHighestSalary({N})': [nth_salary]})
+
