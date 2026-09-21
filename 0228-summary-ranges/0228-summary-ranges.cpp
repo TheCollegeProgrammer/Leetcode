fdaebe5ebbc451bcff7550be475614401d59f954
Devansh Avchat <devanshavchat8208@gmail.com>
2026-09-21 23:07:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (40.6%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
-
         if (nums.empty())
             return ans;
-
         int start = 0;
-
         for (int i = 1; i <= nums.size(); i++) {
-
             // Range break ho gayi
             if (i == nums.size() || nums[i] != nums[i - 1] + 1) {
 
-                if (start == i - 1) {
-                    // Single number
+                if (start == i - 1) { // Single number
                     ans.push_back(to_string(nums[start]));
                 } 
                 else { // Range
